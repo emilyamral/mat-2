b7785998beb3911c5fe6414c1469419f571f636a
emilyamral <165969626+emilyamral@users.noreply.github.com>
2024-06-05 21:33:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
+const caixaPrincipal = document.querySelector(‘.caixa-
+principal’);
+const caixaPerguntas = document.querySelector(‘.caixa-
+perguntas’);
+const caixaAlternativas = document.querySelector(‘.caixa-
+alternativas’);
+const caixaResultado = document.querySelector(‘.caixa-
+resultado’);
+const textoResultado = document.querySelector(‘.texto-
+resultado’);
+const perguntas = []
+const lapis = {
+tamanho: 20,
+tipo: ‘HB’,
+cor: ‘Grafite’,
+temBorrachaAtras: false
+}
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+}
+]
+const perguntas = [ //abre lista de perguntas
+{ //abre objeto de pergunta
+enunciado: “Pergunta 1”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+} //fecha objeto de pergunta
+] //fecha lista de perguntas
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+},
 
+{
+enunciado: “Pergunta 2”,
+alternativas: [
+“Alternativa 1”,
+“Alternativa 2”
+]
+}
+]
